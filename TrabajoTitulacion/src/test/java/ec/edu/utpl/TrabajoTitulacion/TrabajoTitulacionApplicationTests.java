6acd5a96611415b7f098771b764826131f861421
Erika Vasquez <etvasquez@utpl.edu.ec>
2020-07-07 16:08:29 -0500

    Inicializando el proyecto

---
+++ TrabajoTitulacion/src/test/java/ec/edu/utpl/TrabajoTitulacion/TrabajoTitulacionApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.utpl.TrabajoTitulacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoTitulacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
